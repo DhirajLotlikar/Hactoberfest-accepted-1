f374e8fc6a745ece9c9085f1ebfbac8633b8a32e
Prashik <prashikramteke555@gmail.com>
2021-10-06 19:32:56 +0530

    added adding 2 numbers in C++ program

---
+++ CPP/add2numbers.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+ int num1=10,num2=20;
+ //cin>>num1>>num2;
+ int ans;
+ ans = num1+num2;
+
+ cout<<"The sum is "<<ans;
+
+ return 0; 
+}
